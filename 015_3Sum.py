b05ad0f11118258457509caa63aab27c61f43685
sangchul.ku <sangchul.ku@samsung.com>
2019-11-06 16:59:58 +0900

    https://leetcode.com/problems/3sum/

--- 015_3Sum.py
+++ 015_3Sum.py
@@ -12,15 +12,16 @@ class Solution:
             while L < R:
                 _sum = nums[i] + nums[L] + nums[R]
                 if _sum == 0:
-                    ans.append(nums[i], nums[L], nums[R])
-                    ++L, --R
+                    ans.append([nums[i], nums[L], nums[R]])
+                    L += 1
+                    R -= 1
                     while L < R and nums[L] == nums[L - 1]:
-                        ++L
+                        L += 1
                     while L < R and nums[R] == nums[R + 1]:
-                        --R
+                        R -= 1
                 elif _sum < 0:
-                    ++L
+                    L += 1
                 else:
-                    --R
+                    R -= 1
         return ans
         
